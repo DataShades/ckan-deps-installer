4d23a86dd89fae7c43a8065435b2139c1297071c
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2021-04-18 11:48:27 +0700

    add help

--- deps.mk
+++ deps.mk
@@ -1,80 +1,76 @@
-_version = 0.0.13
-_installer_version ?= $(_version)
+_installer_version = 0.0.14
+_version ?= $(_installer_version)
 
 ifneq ($(_installer_version),$(_version))
-$(warning You are using outdated version of installer($(_version) instead of $(_installer_version)))
+$(error You are using incorrect version of installer($(_version) instead of $(_installer_version)))
 endif
 
 root_dir = ..
 vpath ckanext-% $(root_dir)
 
-
 help:
-	@echo Set all extensions to the correct state and reset all customizations
-	@echo -e \\tmake sync [install_extension=true]
+	@echo CKAN dependencies installer
+	@echo Usage:
+	@echo -e '\tmake [target]'
+	@echo TLDR\;
+	@echo -e '\tmake prepare sync install'
 	@echo
-	@echo Check whether extensions are up-to-date
-	@echo -e \\tmake check
+	@echo Targets:
+	@echo -e '\tlist - list all dependencies'
 	@echo
-	@echo Check whether CKAN version is correct
-	@echo -e \\tmake check-ckan
+	@echo -e '\tckanext-NAME - clone the extension if missing and checkout to the required state'
+	@echo -e '\tckanext - perform ckanext-NAME for every single dependency'
 	@echo
-	@echo Download extension and switch it to the required branch
-	@echo -e \\tmake ckanext-NAME \# i.e, make ckanext-scheming
+	@echo -e '\tsync-NAME - clone, update origin, reset changes and checkout the extension'
+	@echo -e '\tsync - perform sync-NAME for every single dependency'
 	@echo
-	@echo Synchronize singe extension
-	@echo -e \\tmake sync-NAME  [install-extension=true] \# i.e, make sync-scheming
+	@echo -e '\tinstall-NAME - install the extension and its pip-requirements'
+	@echo -e '\tinstall - perform install-NAME for every single dependency'
 	@echo
-	@echo Check single extension
-	@echo -e \\tmake check-NAME \# i.e, make check-scheming
+	@echo -e '\tcheck-NAME - check whether the extension is in required state'
+	@echo -e '\tcheck - perform check-NAME for every single dependency'
 	@echo
 
-ckanext-% check-% sync-% install-%: type = $(word 2, $(remote-$(ext)))
-ckanext-% check-% sync-% install-%: remote = $(firstword $(remote-$(ext)))
-ckanext-% check-% sync-% install-%: target = $(lastword $(remote-$(ext)))
 
-install: $(ext_list:%=install-%)
-install-%: ext = $(@:install-%=%)
-install-%: ckanext-%
-	cd $(root_dir)/ckanext-$(ext); \
-	pip install -e.; \
-	for f in requirements.txt pip-requirements.txt; do \
-		echo $$f; \
-		if [ -f "$$f" ]; then pip install -r "$$f"; fi; \
-	done;
+list:
+	@echo $(ext_list)
 
-ckanext: $(ext_list:%=ckanext-%)
-ckanext-%: ext = $(@:ckanext-%=%)
+.SECONDEXPANSION:
+
+install ckanext sync check: $(ext_list:%=$$@-%)
+ckanext-% check-% sync-% install-%: ext_path=$(root_dir)/ckanext-$*
+ckanext-% check-% sync-% install-%: type = $(word 2, $(remote-$*))
+ckanext-% check-% sync-% install-%: remote = $(firstword $(remote-$*))
+ckanext-% check-% sync-% install-%: target = $(lastword $(remote-$*))
+
 ckanext-%:
-	@echo [Clone $(ext) into $(root_dir)/ckanext-$(ext)]
-	cd $(root_dir); \
-	git clone $(remote);
-	cd $(root_dir)/ckanext-$(ext); \
+	@echo [Clone $* into $(ext_path)]
+	git clone $(remote) $(ext_path);
+	cd $(ext_path); \
 	if [ "$(type)" == "branch" ]; then \
 		git checkout -B $(target) origin/$(target); \
 	fi
 
-sync: $(patsubst %,sync-%,$(ext_list))
-sync-%: ext = $(@:sync-%=%)
 sync-%: ckanext-%
-	@echo Synchronize $(ext)
-
-	cd $(root_dir)/ckanext-$(ext); \
+	@echo [Synchronize $*];
+	cd $(ext_path); \
 	git remote set-url origin $(remote); \
 	git fetch origin;
+	cd $(ext_path); \
+	git reset --hard; \
 	if [ "$(type)" == "branch" ]; then \
-		cd $(root_dir)/ckanext-$(ext); \
-		git reset --hard; \
 		git checkout -B $(target) origin/$(target); \
 		git reset --hard origin/$(target); \
 	fi;
-ifneq (,$(install_extension))
-	make install-$(@:sync-%=%)
-endif
 
-check: ckan-check $(ext_list:%=check-%)
-check-%: ext=$(@:check-%=%)
-check-%: ext_path=$(root_dir)/ckanext-$(ext)
+install-%: ckanext-%
+	cd $(ext_path); \
+	pip install -e.; \
+	for f in requirements.txt pip-requirements.txt; do \
+		if [ -f "$$f" ]; then pip install -r "$$f"; fi; \
+	done;
+
+check: ckan-check
 check-%:
 	@if [ ! -d "$(ext_path)" ]; then \
 		echo $(ext_path) does not exist; \
@@ -83,25 +79,25 @@ check-%:
 	cd "$(ext_path)"; \
 	remote_url=$$(git remote get-url origin); \
 	if [ "$$remote_url" != "$(remote)" ]; then \
-		echo $(ext) remote is different from $(remote): $$remote_url; \
+		echo $* remote is different from $(remote): $$remote_url; \
 		exit 0; \
 	fi; \
 	if [ "$(type)" == "branch" ]; then \
 	    branch=$$(git rev-parse --abbrev-ref HEAD); \
 	    if [ "$$branch" != "$(target)" ]; then \
-		    echo $(ext) branch is different from $(target): $$branch; \
+		    echo $* branch is different from $(target): $$branch; \
 		    exit 0; \
 	    fi; \
 	    git fetch origin; \
 	    if [ "$$(git log ..origin/$$branch)" != "" ]; then \
-		    echo $(ext) remote has extra commits; \
+		    echo $* remote has extra commits; \
 		    exit 0; \
 	    fi; \
 	fi; \
-	echo $(ext) is up-to-date;
+	echo $* is up-to-date;
 
 ckan-check:
-		@if [ ! -d "$(root_dir)/ckan" ]; then \
+	@if [ ! -d "$(root_dir)/ckan" ]; then \
 		echo "CKAN is not available at $(root_dir)/ckan"; \
 		exit 0; \
 	fi; \
